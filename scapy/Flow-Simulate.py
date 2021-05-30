5d5d71044c9b4cfc5f59a408cc09d08ca45aa384
xlt200 <63595977+xlt200@users.noreply.github.com>
2021-05-30 17:52:02 +0800

    Update Flow-Simulate.py

--- scapy/Flow-Simulate.py
+++ scapy/Flow-Simulate.py
@@ -1,49 +1,48 @@
+
 from scapy.all import *
 from multiprocessing import Pool
 from multiprocessing import Process
 import time
 
-#建立Flow類
 class Flow:
     
-    def __init__(self, dst, src = '10.0.0.5', time_intv = 3, tos = 0):
+    def __init__(self, dst, src = '10.0.0.5', src_mac = "00:00:00:00:00:05", iface = "server-eth1", time_intv = 1, tos = 0):
         self.src = src
         self.dst = dst
         self.time_intv = time_intv
         self.tos = tos
+        self.src_mac = src_mac
+        self.ether_type = 2048
+        self.iface = iface
 
-        self.AF3 = [104, 112, 120]
-        self.EF = [184]
+        self.__AF3 = [104, 112, 120]
+        self.__EF = [184]
 
     def __packet_init(self):
-        self.__packet = IP()
-        self.__packet.src = self.src
-        self.__packet.dst = self.dst
+        self.__packet = Ether(src = self.src_mac, type = self.ether_type)/IP(src = self.src, dst = self.dst)
         self.__packet.tos = self.tos
     
-    #傳送視頻流量
     def __send_Video(self):
         time_end = time.time() + 10*self.time_intv
         while time.time() < time_end:
-            send(self.__packet, count=800)
+            sendp(self.__packet, count=800, iface=self.iface)
 
-    #傳送音訊流量
     def __send_Voice(self):
         time_end = time.time() + 10*self.time_intv
         while time.time() < time_end:
-            send(self.__packet, count=10)
+            sendp(self.__packet, count=10, iface=self.iface)
     
     def __send_Ndef(self):
         time_end = time.time() + 10*self.time_intv
         while time.time() < time_end:
-            send(self.__packet, count=100)
+            sendp(self.__packet, count=100, iface=self.iface)
            
     def send_flow(self):
         self.__packet_init()
 
-        if self.__packet.tos in self.EF:
+        if self.__packet.tos in self.__EF:
             self.__send_Voice()
-        elif self.__packet.tos in self.AF3:
+        elif self.__packet.tos in self.__AF3:
             self.__send_Video()
         else:
             self.__send_Ndef()
@@ -54,7 +53,7 @@ def sending_flow(flow):
 
 
 if __name__ == "__main__":
-    pro = Pool(processes = 6) # 建立6個進程
+    pro = Pool(processes = 6)
     
     flow1_voice = Flow(dst = '10.0.0.1', tos = 184)
     flow1_video = Flow(dst = '10.0.0.1', tos = 104)
